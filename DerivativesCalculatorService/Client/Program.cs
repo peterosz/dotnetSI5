4c5cb2692541f7ce51e01f90065e9d4a05ca3fb6
peter.osz <itharius@gmail.com>
2018-01-06 18:05:33 +0100

    Exercise 3 done

---
+++ DerivativesCalculatorService/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key when the service is ready!");
+            Console.ReadKey();
+
+            DerivativesCalculatorClient proxy = new DerivativesCalculatorClient("DerivativesCalculatorConfiguration");
+            decimal result = proxy.CalculateDerivative(3, new string[] { "Codecool Halleluja" }, new string[] { });
+            Console.WriteLine($"Result: {result}");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
